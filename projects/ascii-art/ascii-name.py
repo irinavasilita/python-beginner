3283eabcbd1de24bdf143816595e105d049999fb
Irina <irinavasilitav@gmail.com>
2023-01-30 17:29:19 +0200

    Upload first project

---
+++ projects/ascii-art/ascii-name.py
@@ -0,0 +1,14 @@
+# First Name: Irina
+# Fun fact: I did breakdance for 4 years.
+
+myName = """
+IIIII  RRRR   IIIII  N   N    A  
+  I    R   R    I    NN  N   A A 
+  I    R   R    I    N N N  A   A
+  I    RRRR     I    N  NN  AAAAA
+  I    R R      I    N   N  A   A
+  I    R  R     I    N   N  A   A
+IIIII  R   R  IIIII  N   N  A   A
+"""
+
+print(myName)
